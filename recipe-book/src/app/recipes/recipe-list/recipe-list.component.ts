fb427c8c0a28c4949466fc13890f6bbf9a60e8ab
Naveen Pete <naveen.shenoy.blr@gmail.com>
2016-11-23 22:30:58 +0530

    add directives to the app

--- recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,11 +6,28 @@ import { Recipe } from '../recipe';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Masala Dosa', 
+      'Masala dosa or Masale Dose is a variant of the popular South Indian food dosa and has its origins in Tuluva Mangalorean cuisine made popular by the Udupi hotels all over India.', 
+      '/assets/images/masala-dosa.jpg',
+      []),
+
+    new Recipe('Neer Dosa', 
+      'Neer dosa or Neer dose literally meaning Water dosa is a crêpe prepared from rice batter. It is light type of dosa, an Indian dish.', 
+      '/assets/images/neer-dosa.jpg',
+      []),
+
+    new Recipe('Rava Idli', 
+      'Rava idli is a variation of the popular South Indian breakfast item, idli, made with rava or Bombay rava.', 
+      '/assets/images/rava-idli.jpg',
+      []),
+
+    new Recipe('Puliyogare', 
+      'Pulihora, Puliyodarai, Puliyogare or simply Tamarind Rice is a common rice preparation in the South Indian states of Andhra Pradesh, Telangana, Karnataka and Tamil Nadu.', 
+      '/assets/images/puliyogare.jpg',
+      [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Masala Dosa', 
-    'Dosa is a kind of pancake made from a fermented batter.', 
-    '/assets/images/masala-dosa.jpg');
 
   constructor() { }
 
