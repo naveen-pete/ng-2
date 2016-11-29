700208b544bd4d38bf18e9b870234caf61be5b8b
Naveen Pete <naveen.shenoy.blr@gmail.com>
2016-11-29 19:52:26 +0530

    add forms and validations

--- recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ recipe-book/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,14 +1,114 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/rx';
+import { FormArray, FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
+import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  recipeForm: FormGroup;
+  private recipeIndex: number;
+  private recipe: Recipe;
+  private subscription: Subscription;
+  private isNew = true;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, 
+              private service: RecipeService,
+              private formBuilder: FormBuilder,
+              private router: Router) { }
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        if(params.hasOwnProperty('id')) {
+          this.isNew = false;
+          this.recipeIndex = +params['id'];   // cast from string to number using +
+          this.recipe = this.service.getRecipe(this.recipeIndex);
+        } else {
+          this.isNew = true;
+          this.recipe = null;
+        }
+        this.initForm();
+      }
+    );
+    console.log('isNew:', this.isNew);
+  }
+
+  onSubmit() {
+    let newRecipe = this.recipeForm.value;
+    if(this.isNew) {
+      this.service.addRecipe(newRecipe);
+    } else {
+      this.service.editRecipe(this.recipe, newRecipe);
+    }
+    this.navigateBack();
+  }
+
+  onAddItem(name: string, amount: string) {
+    let ingredients: FormArray = <FormArray>this.recipeForm.controls['ingredients'];
+    ingredients.push(
+      new FormGroup({
+        name: new FormControl(name, Validators.required),
+        amount: new FormControl(amount, [
+          Validators.required,
+          Validators.pattern('\\d+')
+        ])
+      })
+    );
+  }
+
+  onRemoveItem(index: number) {
+    let ingredients: FormArray = <FormArray>this.recipeForm.controls['ingredients'];
+    ingredients.removeAt(index);
+  }
+
+  onCancel() {
+    this.navigateBack();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private navigateBack() {
+    this.router.navigate(['../']);
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeIngredients: FormArray = new FormArray([]);
+
+    if(!this.isNew) {
+      for(let i=0; i < this.recipe.ingredients.length; i++) {
+        recipeIngredients.push(
+          new FormGroup({
+            name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
+            amount: new FormControl(this.recipe.ingredients[i].amount, [
+              Validators.required,
+              Validators.pattern('\\d+')
+            ])
+          })
+        );
+      }
+      recipeName = this.recipe.name;
+      recipeImagePath = this.recipe.imagePath;
+      recipeDescription = this.recipe.description;
+    }
+    
+    this.recipeForm = this.formBuilder.group({
+      name: [recipeName, Validators.required],
+      imagePath: [recipeImagePath, Validators.required],
+      description: [recipeDescription, Validators.required],
+      ingredients: recipeIngredients
+    });
+
   }
 
 }
--- recipe-book/src/app/shopping-list/shopping-list-add.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list-add.component.ts
@@ -1,14 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnChanges {
+  @Input() item: Ingredient;
+  @Output() cleared = new EventEmitter();
+  isAdd = true;
 
-  constructor() { }
+  constructor(private service: ShoppingListService) { }
 
-  ngOnInit() {
+  ngOnChanges(changes) {
+    if(changes.item.currentValue === null) {
+      this.isAdd = true;
+      this.item = { name: null, amount: null };
+    } else {
+      this.isAdd = false;
+    }
+  }
+
+  onSubmit(ingredient: Ingredient) {
+    let newIngredient = new Ingredient(ingredient.name, ingredient.amount);
+    if(this.isAdd) {
+      this.item = newIngredient;
+      this.service.addItem(this.item);
+    } else {
+      this.service.editItem(this.item, newIngredient);
+      this.onClear();
+    }
+  }
+
+  onDelete() {
+    this.service.deleteItem(this.item);
+    this.onClear();
+  }
+
+  onClear() {
+    this.isAdd = true;
+    this.cleared.emit(null);
   }
 
 }
